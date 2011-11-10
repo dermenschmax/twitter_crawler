323dc1137a5a5da407f09f45a06b615afd58a108
dev <dev@development.local>
2011-11-10 12:13:50 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterCrawler::Application.config.secret_token = 'e8a42aab28e79180ada34af2283ea79748a061084eaa3a277889451dc07c01b2c40239d2db4753eb0172c1f10f543bb0249eadbe711a7c982bff928a9a771724'
