e7781b677f610b8e94f40e181d446e8b64b1669f
dev <dev@development.local>
2011-11-21 21:26:58 +0100

    js immer noch nicht

--- app/assets/javascripts/twitter_crawler.js
+++ app/assets/javascripts/twitter_crawler.js
@@ -1,16 +1,17 @@
-
-function ule_expose(){
+function ule_expose(e){
     alert("Hallo");
 }
 
 
 
-#// registiert die notwendigen Event-Handler beim Laden der Seite
+// registiert die notwendigen Event-Handler beim Laden der Seite
 function reg_events(){
     
   // ---- user_listing_entry ----
-  $('.ule_expose').click(ule_expose);
-   
+  $('.lnk').click(function() { alert('inline');});
+  $('div').click(function() {
+        alert('Handler for .click() called.');
+    });
 }
 
 
