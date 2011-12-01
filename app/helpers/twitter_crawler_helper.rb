dc5c6cef545df660e66dc61a7509a21708bf1c71
dev <dev@development.local>
2011-12-01 18:24:05 +0100

    mentions helper

--- app/helpers/twitter_crawler_helper.rb
+++ app/helpers/twitter_crawler_helper.rb
@@ -26,5 +26,33 @@ module TwitterCrawlerHelper
   def join_mentions(user_mentions)
     user_mentions.collect{|um| "@"+um.screen_name}.join(" ; ") unless user_mentions.nil?
   end
+  
+  
+  # ------------------------------------------------------------------
+  # Verlinkt die user_mentions in dem übergebenen String.
+  #
+  # Parameter:
+  #   text    der zu verlinkende Text (der tweet)
+  #
+  # Rückgabe:
+  #   der neue Text mit links drin
+  #
+  # Test
+  #   twitter_crawler_helper_test::link_mention_helper
+  # ------------------------------------------------------------------
+  def link_mentions_helper(text)
+    s = ""
+    unless (text.nil?)
+      text.lines(" ") do |l|
+        if (!l.nil? && l.size > 0 && l[0] == "@") then
+          s += "<a href='#'>#{l.strip}</a> "
+        else
+          s += l
+        end
+      end
+    end
+    
+    s
+  end
 
 end
--- test/unit/helpers/twitter_crawler_helper_test.rb
+++ test/unit/helpers/twitter_crawler_helper_test.rb
@@ -1,4 +1,39 @@
 require 'test_helper'
 
 class TwitterCrawlerHelperTest < ActionView::TestCase
+  
+  test "kein_mention" do
+    
+    # 1. kein Mention: t == s
+    t = "Hallo, kein Mention"
+    s = link_mentions_helper(t)
+    
+    assert_not_nil(s)
+    assert_equal(s, t, "kein mention enthalten '#{t}' != '#{s}'")
+  end
+  
+  test "ein_mention"do
+    t = "Hallo, ein @Mention im Text"
+    s = link_mentions_helper(t)
+    
+    assert_not_nil(s)
+    assert_not_equal(s, t, "#{t} soll einen Link enthalten '#{s}'")
+    assert(s.index("<a href") >= 0)
+    assert(s.index("</a>") > 0)
+  end
+
+  test "nur_mention" do
+    t = "@Mention"
+    s = link_mentions_helper(t)
+    
+    assert_not_nil(s)
+    assert_not_equal(s, t, "#{t} erhaelt einen Link '#{s}'")
+    assert(s.index("<a") >= 0, "Link-Anfang fehlt #{s}")
+    assert(s.index("</a>") > 0, "Link-Ende fehlt #{s}")
+  end
+  
+  test "zwei_mentions" do
+    assert(false)
+  end
+  
 end
